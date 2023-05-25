971d256b21052cf7d4f5f3824f87a93a1e1ef1c5
elecshen <51473599+elecshen@users.noreply.github.com>
2023-05-25 22:09:24 +0700

    Добавление представлений

--- AoCP/Program.cs
+++ AoCP/Program.cs
@@ -19,7 +19,7 @@ namespace AoCP
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();

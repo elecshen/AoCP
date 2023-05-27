f28849f8dfd59a315ad0b7e527f24c9a1657a954
Boomrock <63188345+Boomrock@users.noreply.github.com>
2023-05-28 00:15:42 +0700

    add LevenshteinDistance

---
+++ AoCP/Models/DataBaseModel/AplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AoCP.Models.DataBaseModel
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Object> Object { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=178.170.193.230;user=root;password=Sji-quV-5Mm-XPv;database=aocp;",
+                new MySqlServerVersion(new Version(8, 0, 25)));
+        }
+    }
+}
